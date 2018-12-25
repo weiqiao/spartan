26a0c5b6f42fa228b6d4dcb8fd411f982e6ebb55
Weiqiao Han <weiqiaoh@mit.edu>
2018-12-25 15:14:15 -0500

    ar tags working

---
+++ workspace/ros_home/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/weiqiao/spartan/workspace/ros_home/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/home/weiqiao/spartan/build/install/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"
+export PATH="/opt/ros/kinetic/bin:/home/weiqiao/spartan/build/install/bin:/home/weiqiao/spartan/scripts/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export PWD="/home/weiqiao/spartan/workspace/ros_home/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages:/home/weiqiao/spartan/build/install/lib/python2.7/site-packages:/home/weiqiao/spartan/build/install/lib/python2.7/dist-packages:/home/weiqiao/spartan/modules"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/weiqiao/spartan/workspace/ros_home/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/weiqiao/spartan/workspace/ros_home/src:/opt/ros/kinetic/share"
